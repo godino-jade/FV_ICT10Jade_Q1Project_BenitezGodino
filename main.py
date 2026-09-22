7a83d49b1a74e201aebf888cf48fa9ef34ff02b5
godino-jade <godino.jam@obmontessori.edu.ph>
2026-09-22 20:19:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyscript import document, display
 
-def display_char(e):
-    document.getElementById("output2").innerHTML = " "  # clears previous result
+def create_order(e):
+    document.getElementById("output1").innerHTML = " "  # clears previous result
 
     # Get each item element
     prod1 = document.getElementById("item1")
@@ -9,8 +9,6 @@ def display_char(e):
     prod3 = document.getElementById("item3")
     prod4 = document.getElementById("item4")
     prod5 = document.getElementById("item5")
- 
-
 
     # Calculate subtotal 
     subtotal = (float(prod1.value) * prod1.checked
@@ -19,17 +17,14 @@ def display_char(e):
                 + float(prod4.value) * prod4.checked
                 + float(prod5.value) * prod5.checked)       
 
-    
-    
-
     # Tax and price with tax
-    tax_rate = 0.12  
-    tax = subtotal * tax_rate
+    tax_rate = 0.12 # VAT OF 12%
+    tax = subtotal * tax_rate # order total + tax
     
     # Grand total
-    total = subtotal + size_price + tax + 
+    total = subtotal + tax 
 
-    display(f"Total: P{total}.", target="output1")
-    display(f"Subtotal: P{subtotal}.", target="output1")
-    display(f"Tax: P{tax}.", target="output1", append=True)
-    display(f"Thank you for your order!", target="output1", append=True)
+    display(f"Total: ₱{total}.", target="output1", append=True)
+    display(f"Subtotal: ₱{subtotal}.", target="output1", append=True)
+    display(f"Tax: ₱{tax}.", target="output1", append=True)
+    display(f"Thank you for your order!", target="output2", append=True)
